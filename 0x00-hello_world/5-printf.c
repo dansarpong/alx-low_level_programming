6b34096ee240503d37fd1fcdca4d154915cb925d
dansarpong <dansarpong.me@gmail.com>
2023-09-07 15:42:21 -0700

    Fix c files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+  4   * main - main function
+  5   *
+  6   *Return: 0 (Success)
+  7   */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+  * main - main function
+  *
+  *Return: 0 (Success)
+  */
 int main(void)
 {
 	int char_size = sizeof(char);
@@ -9,10 +14,10 @@ int main(void)
 	int float_size = sizeof(float);
 
 	printf("Size of a char: %d byte(s)\n", char_size);
-	printf("Size of a int: %d byte(s)\n", int_size);
+	printf("Size of an int: %d byte(s)\n", int_size);
 	printf("Size of a long int: %d byte(s)\n", long_size);
 	printf("Size of a long long int: %d byte(s)\n", long2_size);
 	printf("Size of a float: %d byte(s)\n", float_size);
 
-	return 0;
+	return (0);
 }
