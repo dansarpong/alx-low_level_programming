ff1668d0cbe2cbf76085d20061fbba462218649d
dansarpong <dansarpong.me@gmail.com>
2023-09-07 15:46:14 -0700

    Fix c files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-  4   * main - main function
-  5   *
-  6   *Return: 0 (Success)
-  7   */
+  * main - main function
+  *
+  *Return: 0 (Success)
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-  4   * main - main function
-  5   *
-  6   *Return: 0 (Success)
-  7   */
+  * main - main function
+  *
+  *Return: 0 (Success)
+  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
