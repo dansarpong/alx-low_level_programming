469772faea04dfce10d2162656cfad5785b058db
dansarpong <dansarpong.me@gmail.com>
2023-09-07 15:36:31 -0700

    Do task 1-6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int char_size = sizeof(char);
+	int int_size = sizeof(int);
+	int long_size = sizeof(long int);
+	int long2_size = sizeof(long long int);
+	int float_size = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", char_size);
+	printf("Size of a int: %d byte(s)\n", int_size);
+	printf("Size of a long int: %d byte(s)\n", long_size);
+	printf("Size of a long long int: %d byte(s)\n", long2_size);
+	printf("Size of a float: %d byte(s)\n", float_size);
+
+	return 0;
+}
