6d034c81bd135e2f5bc97a05f4f9f064963eb20f
dansarpong <dansarpong.me@gmail.com>
2023-09-08 16:00:59 -0700

    Do task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - print single digit numbers
+  *
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		printf(c++);
+	}
+	putchar('\n';
+	return (0);
+}
