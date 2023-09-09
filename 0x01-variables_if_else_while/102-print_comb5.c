ee2888548253999699e0747b4fc2ba23046a2e4e
dansarpong <dansarpong.me@gmail.com>
2023-09-08 17:22:51 -0700

    Do task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+  * main - print all possible combinatioins of
+  * two two-digit numbers
+  *
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+	int c;
+	int d;
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			c = a;
+			d = b + 1;
+			while (c <= '9')
+			{
+				while (d <= '9')
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a != '9' && b != '8' && c != '9' && d != '9')
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
