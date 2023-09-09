bacce6d894f106af0f20de39e8a82eb8fa10abcc
dansarpong <dansarpong.me@gmail.com>
2023-09-08 17:00:07 -0700

    Do task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  * main - print all possible combinations
+  * of three digits
+  *
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int first = '0';
+	int second;
+	int third;
+
+	while (first <= '7')
+	{
+		second = first + 1;
+		while (second <= '8')
+		{
+			third = second + 1;
+			while (third <= 9)
+			{	
+				putchar(first);
+				putchar(second);
+				putchar(third);
+				if (first == '7' && second == '8' && third == '9')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+				third++;
+			}
+			second++;
+		}
+		first++;
+	}
+	putchar('\n');
+	return (0);
+}
