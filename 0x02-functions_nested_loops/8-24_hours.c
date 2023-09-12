c277ecb9ab52decea3d5469a147f12d0f39512fa
dansarpong <dansarpong.me@gmail.com>
2023-09-12 15:44:07 -0700

    Fix task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
 			_putchar(':');
-			_putchar((hours / 10) + '0');
+			_putchar((minutes / 10) + '0');
 			_putchar((minutes % 10) + '0');
 			_putchar('\n');
 		}
