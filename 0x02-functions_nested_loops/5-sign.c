9547636aec970027ce71f41f3f93f030927efb28
dansarpong <dansarpong.me@gmail.com>
2023-09-12 14:38:23 -0700

    Do task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_sign - print the sign of a number
+  * @n: number to be checked
+  * Return: 1 if n is greater than 0,
+  * 0 if n is 0,
+  * -1 if n is less than 0
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	elif (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
