860d965a577a6409633029f01ac664c2b22b60d6
dansarpong <dansarpong.me@gmail.com>
2023-09-12 14:49:07 -0700

    Do task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+   * _abs - compute absolute value
+   * @int: value being computed
+   * Return: absolute value
+   */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
