6132ae4576e6c004f144945bcccce0c5532bee3a
dansarpong <dansarpong.me@gmail.com>
2023-09-14 15:37:22 -0700

    Do task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_number - print an integer
+  * @n: int to print
+  */
+
+void print_number(int n)
+{
+	int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+		num = n;
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
