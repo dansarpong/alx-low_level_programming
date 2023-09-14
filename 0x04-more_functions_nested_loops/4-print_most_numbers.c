16c73e592ac1000e3644af4a1e66d84cd49f4373
dansarpong <dansarpong.me@gmail.com>
2023-09-14 13:23:40 -0700

    Do task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_numbers - print from 0 to 9
+  */
+void print_numbers(void)
+{
+	char i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
