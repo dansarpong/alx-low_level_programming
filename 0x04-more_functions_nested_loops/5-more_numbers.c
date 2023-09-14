efc6f1da67921e0715e7b0905f10fa3d173ebeea
dansarpong <dansarpong.me@gmail.com>
2023-09-14 13:43:43 -0700

    Do task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * more_numbers - print 10 times from 0 to 14
+  */
+
+void more_numbers(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c < 10)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
