039dd0f3d55becfc9b0c13d89df5d2f4213b0b73
dansarpong <dansarpong.me@gmail.com>
2023-09-18 14:48:03 -0700

    Do task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_array - print n elements of an array of integers
+  * @a: array of integers
+  * @n: number of elements to print
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if ((i + 1) < n)
+			printf(", ");
+	}
+	_putchar('\n');
+}
