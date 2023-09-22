116efd7ffcba7c756d04e8ae36d9af9ea73416ac
dansarpong <dansarpong.me@gmail.com>
2023-09-22 15:09:11 -0700

    Do task 6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * string_toupper - change all lowercase letters to a string to uppercase
+  * @c: string to check
+  * Return: @c
+  */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'A' && c[i] <= 'Z')
+			c[i] = (c[i] - 'A') + 'a';
+	}
+	return (c);
+}
