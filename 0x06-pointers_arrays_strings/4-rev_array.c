4ab72d5a109f2dcb92c1b072a5012f7e4750ac5e
dansarpong <dansarpong.me@gmail.com>
2023-09-22 14:58:32 -0700

    Do task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * reverse_array - reverses the content of an array of integers
+  * @a: array to reverse
+  * @n: number of elements of the array
+  */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	--n;
+	for (i = 0; i < n; i++, n--)
+	{
+		temp = a[i];
+		a[i] = a[t];
+		a[t] = temp;
+	}
+}
