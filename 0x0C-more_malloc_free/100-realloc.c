1cff4c98c443d0232364c09ff7bc2792db4334fd
dansarpong <dansarpong.me@gmail.com>
2023-10-05 22:35:17 -0700

    Do task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,5 +30,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < old_size &&  i < new_size; i++)
 		*((int *)(p) + i) = *((int *)(ptr) + i);
+	free(ptr);
 	return (p);
 }
