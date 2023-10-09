abafdbe7c265544e789cfc27282b82d1b032c705
dansarpong <dansarpong.me@gmail.com>
2023-10-09 15:50:03 -0700

    Do task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+  * struct dog - struct for dog
+  * @name: name
+  * @age: age
+  * @owner: owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
