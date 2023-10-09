dbf472485bd54b46a59ff1dcf86f00aebbcf772c
dansarpong <dansarpong.me@gmail.com>
2023-10-09 16:56:50 -0700

    Do task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * new_dog - creates a new dog
+  * @name: name
+  * @age: age
+  * @owner: owner
+  * Return: pointer to new dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}
