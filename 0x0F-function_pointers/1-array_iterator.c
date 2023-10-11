4c5fa30e644deb57d87c44947f2cfd1ccab9ba7f
dansarpong <dansarpong.me@gmail.com>
2023-10-11 10:36:53 -0700

    Do task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - execute a function on all elements of an array
+  * @array: array
+  * @size: size of the array
+  * @action: pointer to the function used
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i, j = (int)size;
+
+	for (i = 0; i < j; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
+#include <stddef.h>
 #ifndef FUNC_POINTERS
 #define FUNC_POINTERS
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
