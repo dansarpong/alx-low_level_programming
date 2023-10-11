b3cd6c8a1d163ec1f84a669c076c1bb4c76d14db
dansarpong <dansarpong.me@gmail.com>
2023-10-11 10:39:55 -0700

    Fix task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i, j = (int)size;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (i = 0; i < j; i++)
 		action(array[i]);
 }
