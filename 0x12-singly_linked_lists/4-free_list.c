461747f8a227896d137048ecfc4a74333789756f
dansarpong <dansarpong.me@gmail.com>
2023-10-19 13:41:12 -0700

    Do task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * free_list - frees a list_t list
+  * @head: pointer to head
+  */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
