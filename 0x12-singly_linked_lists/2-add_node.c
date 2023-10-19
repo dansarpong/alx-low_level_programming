310d7ac80fa19a3f83cbf4b06ccf0614c9a8acd3
dansarpong <dansarpong.me@gmail.com>
2023-10-19 12:12:55 -0700

    Fix task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,12 +14,21 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (!new)
 		return (NULL);
-	
-	new->str = strdup(str);
-	for (; new->str[i] != '\0'; i++)
-		;
-	new->len = i;
+
+	if (!str)
+	{
+		new->str = NULL;
+		new->len = 0;
+	}
+	else
+	{
+		new->str = strdup(str);
+		for (; new->str[i] != '\0'; i++)
+			;
+		new->len = i;
+	}
 	new->next = *head;
+	*head = new;
 
 	return (new);
 }
