fda3956683ecccee04c0ab7ca0e678c9967d782b
dansarpong <dansarpong.me@gmail.com>
2023-10-19 11:13:20 -0700

    Do task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * list_len - find number of elements in a linked list_t list
+  * @h: pointer to head
+  * Return: number of linked lists
+  */
+size_t list_len(const list_t *h)
+{
+	if (!h)
+		return (0);
+
+	return (1 + h->next);
+}
