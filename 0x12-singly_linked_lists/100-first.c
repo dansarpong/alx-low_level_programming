b0aa05fb6e689a7271720f20e5ba539095196068
dansarpong <dansarpong.me@gmail.com>
2023-10-19 14:01:33 -0700

    Do task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
 /**
   * first - gets called before main
   */
-void first() __attribute__((constructor))
+void __attribute__((constructor)) first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
