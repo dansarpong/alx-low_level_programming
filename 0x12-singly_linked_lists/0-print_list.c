94784435e7704edc143bc12442d52baa8585bb2c
dansarpong <dansarpong.me@gmail.com>
2023-10-19 11:00:52 -0700

    Do task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * print_list - print all the elements of a list_t list
+  * @h: pointer to header
+  * Return: number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	int num;
+
+	if (!h)
+		return (0);
+
+	if (!(h->str))
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+
+	return (1 + print_list(h->next));
+}
