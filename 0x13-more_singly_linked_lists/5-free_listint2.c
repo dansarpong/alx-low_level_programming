5c6e2968e782e48b6d6b196fda32ad5118f7b20e
dansarpong <dansarpong.me@gmail.com>
2023-10-23 16:33:40 -0700

    Do task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a listint_t list
+  * @head: pointer to head of list
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
