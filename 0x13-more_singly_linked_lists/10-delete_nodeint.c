b4cddc8b643d8b3fdb6dae7e38e1c96357ebd8a4
dansarpong <dansarpong.me@gmail.com>
2023-10-24 05:30:09 -0700

    Do task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+  * delete_nodeint_at_index - delete node at index
+  * @head: pointer to head
+  * @index: index
+  * Return: 1 if successful, -1 if it failed
+  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *trck;
+	unsigned int i;
+
+	if (!(*head))
+		return (-1);
+
+	if (index == 0)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+
+	trck = *head;
+	for (i = 0; (i < index) && trck; i++)
+		trck = trck->next;
+
+	if (trck && trck->next)
+	{
+		tmp = trck->next;
+		trck->next = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	return (-1);
+}
