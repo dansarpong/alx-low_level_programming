77c8f14dcd0222a14720324d2ea017a7280ddb09
dansarpong <dansarpong.me@gmail.com>
2023-10-26 15:36:05 -0700

    Fix task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '1' || b[i] != '0')
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
 		res = (res << 1) + (b[i] - '0');
 	}
