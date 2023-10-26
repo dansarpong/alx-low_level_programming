ad2c3167bfaaa23b3e40b3895f3573d485c5a28c
dansarpong <dansarpong.me@gmail.com>
2023-10-26 16:11:02 -0700

    Do task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_binary - prints the binary of a number
+  * @n: the number
+  */
+void print_binary(unsigned long int n)
+{
+	unsigned int res;
+	int mask = 1;
+
+	while (mask < n)
+		mask <<= 1;
+
+	while (mask)
+	{
+		res = (mask & n) ? 1 : 0;
+		_putchar(res + '0');
+		mask >>= 1;
+	}
+}
