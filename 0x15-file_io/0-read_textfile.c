97d8b94bb7d64aeac907250071692bf88c6c10f6
dansarpong <dansarpong.me@gmail.com>
2023-10-30 08:38:52 -0700

    Fix task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -10,28 +9,37 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file, bytesread;
-	char buffer[letters];
+	char *buffer = malloc(sizeof(char) * letters);
 
 	if (!filename)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	file = open(filename, O_RDONLY);
 	if (file == -1)
+	{
+		free(buffer);
 		return (0);
-	
+	}
+
 	bytesread = read(file, buffer, letters);
 	if (bytesread == -1)
 	{
+		free(buffer);
 		close(file);
 		return (0);
 	}
 
 	if (write(STDOUT_FILENO, buffer, bytesread) != bytesread)
 	{
+		free(buffer);
 		close(file);
 		return (0);
 	}
 
+	free(buffer);
 	close(file);
 	return (bytesread);
 }
