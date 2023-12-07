69ab36e1e21ef8f709510e779d13e8673cad157a
Daniel Sarpong <dansarpong.me@gmail.com>
2023-12-07 18:42:07 +0000

    Fix task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,8 +15,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
-	*head = new;
+	if (*head)
+	{
+		new->next = *head;
+		(*head)->prev = new;
+	}
+	else
+		new->next = NULL;
 
+	*head = new;
 	return (new);
 }
