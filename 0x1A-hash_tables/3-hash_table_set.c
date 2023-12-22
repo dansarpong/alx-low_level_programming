31b49d1e2ea8918769e300735723701cca672680
dansarpong <dansarpong.me@gmail.com>
2023-12-22 15:24:00 +0000

    Do task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - adds an element to the hash table
+  * @ht: hash table you want to add or update the key/value to
+  * @key: the key (cannot be empty)
+  * @value: value associated with the key (cannot be empty)
+  * Return: 1 if success, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, target;
+	unsigned long int index;
+
+	if (!key || !value || !ht)
+		return (0);
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+
+	index = key_index(key, ht->size);
+	if (!ht->array[index])
+		ht->array[index] = new;
+	else
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
+
+	return (1);
+}
